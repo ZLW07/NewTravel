5bcfb798dc860f6f3e0f69627669170df3206801
wei <205430509@qq.com>
2022-06-09 20:01:24 +0800

    feat (QTOpengL)

--- RobotSimulationPlatform/robot_main.cpp
+++ RobotSimulationPlatform/robot_main.cpp
@@ -1,20 +1,3 @@
-//
-// Created by wei on 2022/5/21.
-//
-
-//#include"myopenglwiegt.h"
-//#include <QApplication>
-//
-//int main(int argc, char *argv[])
-//{
-//    QApplication a(argc, argv);
-//    Widget w;
-//    w.show();
-//    w.resize(800, 600);
-//
-//    return a.exec();
-//}
-
 
 #include "robot_simulation.h"
 #include <QApplication>
