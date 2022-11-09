185102bc5a773723d0658d2a7090df20e5f98153
ZLW <205430509@qq.com>
2022-11-09 08:03:36 +0800

    Test

--- Cmake/LibraryTarget.cmake
+++ Cmake/LibraryTarget.cmake
@@ -1,22 +1,4 @@
 
-#the first way to add library
-
-#set(Cout_DIR ${CMAKE_SOURCE_DIR}/Cout)
-#message(STATUS "Source path is = ${Cout_DIR}")
-#add_library(Cout SHARED ${Cout_DIR}/Cout_Test.cpp)
-
-#the second way to add library
-#add_library(Cout SHARED ${MODULE_SRC})
-
-#the third way to add library which use the function user defined
-#set(Cout_DIR Cout)
-#build_library(Cout Cin)
-#
-#set(Cin_DIR Cin)
-#build_library(Cin)
-#set(GoogleTest_DIR ExternalSource/googletest)
-#build_library(GoogleTest)
-
 set(Log_DIR Log)
 build_library(Log)
 
