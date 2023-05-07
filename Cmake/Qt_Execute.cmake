b9bd851d059e27a5c2a19c5356c139ea09d301b8
zlw07 <205430509@qq.com>
2023-05-07 20:51:20 +0800

    fix(delete qt)

--- Cmake/Qt_Execute.cmake
+++
@@ -1,6 +0,0 @@
-set(UT_QT_DIR Test/Qt)
-qt_execute(UT_QT GL GLU Model Math Log)
-
-
-set(RobotSimulation_DIR RobotSimulationPlatform)
-qt_execute(RobotSimulation GL GLU Model Math Log Configure TinyXML)
